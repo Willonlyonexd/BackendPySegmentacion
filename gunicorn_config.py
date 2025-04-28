a0daf09c28a70f8b65dd8ef41c725448c7bbfe3c
Willonlyonexd <houwenvt@gmail.com>
2025-04-28 14:52:38 -0400

    datos para tabla 2

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 # Configuración para Gunicorn en producción
-workers = 1  # Para aplicaciones de ML, un worker suele ser suficiente
+workers = 4  # Para aplicaciones de ML, un worker suele ser suficiente
 threads = 2
 timeout = 200  # Mayor timeout para procesos de ML que pueden tardar
 max_requests = 1000

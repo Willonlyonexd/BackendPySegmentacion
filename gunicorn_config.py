244e8588287666e2250c6aff6a422ee051e12393
Willonlyonexd <houwenvt@gmail.com>
2025-04-29 11:25:11 -0400

    segmentacion final v1

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 # Configuración para Gunicorn en producción
 workers = 4  # Para aplicaciones de ML, un worker suele ser suficiente
 threads = 2
-timeout = 200  # Mayor timeout para procesos de ML que pueden tardar
+timeout = 500 # Mayor timeout para procesos de ML que pueden tardar
 max_requests = 1000
 max_requests_jitter = 50
 

2552009fcda37fcfae8ef9f634425afc8defb35a
Willonlyonexd <houwenvt@gmail.com>
2025-04-29 11:39:44 -0400

    segmentacion final v3

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
-workers = 4
+workers = 5
 threads = 2
-timeout = 1000  # Aumentar a 1000 segundos (16.6 minutos)
+timeout = 5000  # Aumentar a 1000 segundos (16.6 minutos)
 max_requests = 1000
 max_requests_jitter = 50
